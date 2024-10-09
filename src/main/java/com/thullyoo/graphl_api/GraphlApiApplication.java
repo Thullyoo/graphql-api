cd5c27c22a49ec1a0cf1ec097dedaec7760f3203
Thullyoo <thullyofilho@gmail.com>
2024-10-09 14:44:38 -0300

    Initial commit

---
+++ src/main/java/com/thullyoo/graphl_api/GraphlApiApplication.java
@@ -0,0 +1,13 @@
+package com.thullyoo.graphl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thullyoo/graphl_api/GraphlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyoo.graphl_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
